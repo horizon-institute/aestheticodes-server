9234ecc6faff6fddb859c7d7df9597dc436fa621
Kevin Glover <ktg@cs.nott.ac.uk>
2016-07-22 11:24:27 +0100

    Add support for older apps

---
+++ src/main/java/uk/ac/horizon/artcodes/server/OldExperienceServlet.java
@@ -0,0 +1,53 @@
+/*
+ * Artcodes recognises a different marker scheme that allows the
+ * creation of aesthetically pleasing, even beautiful, codes.
+ * Copyright (C) 2013-2015  The University of Nottingham
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.horizon.artcodes.server;
+
+import com.google.api.client.util.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OldExperienceServlet extends HttpServlet
+{
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse resp) throws IOException
+	{
+		resp.setContentType("application/json");
+		resp.setCharacterEncoding("UTF-8");
+
+		InputStream in = ArtcodeServlet.class.getResourceAsStream("/old_experience_list.json");
+		OutputStream out = resp.getOutputStream();
+		IOUtils.copy(in, out);
+		in.close();
+		out.close();
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		doGet(req, resp);
+	}
+}
